e2d87a452c73cfc49f2529864f320e10c78088be
EliotPu <78807845+EliotPu@users.noreply.github.com>
2023-07-09 10:48:04 +0300

    build(webpack-prod): add "terser-webpack-plugin"

--- webpack/webpack.prod.cjs
+++ webpack/webpack.prod.cjs
@@ -1,12 +1,25 @@
 // @ts-check
 console.log('webpack-prod-js');
 
+const TerserPlugin = require('terser-webpack-plugin');
+
 const config = {
   mode: 'production',
 
   output: {
     filename: '[name].[contenthash].bundle.mjs',
     chunkFilename: '[name].[contenthash].mjs',
+  },
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          /* https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions */
+        },
+      }),
+    ],
   },
 };
 
