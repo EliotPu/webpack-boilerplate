73c542fe0539a182574f60c8744232de0ab78000
EliotPu <78807845+EliotPu@users.noreply.github.com>
2023-07-10 12:12:35 +0300

    build(webpack-dev): add "devServer" config

--- webpack/webpack.dev.cjs
+++ webpack/webpack.dev.cjs
@@ -1,9 +1,55 @@
 // @ts-check
 console.log('webpack-dev-js');
 
+const { resolve } = require('path');
+
 const config = {
   mode: 'development',
   devtool: 'inline-source-map',
+
+  output: {
+    filename: '[name].[fullhash:8].bundle.mjs',
+    chunkFilename: '[name].[chunkhash:8].mjs',
+  },
+
+  devServer: {
+    open: true,
+    hot: true,
+    server: 'http',
+    compress: true,
+    historyApiFallback: true,
+    port: 3000,
+    allowedHosts: 'auto',
+
+    watchFiles: [resolve(__dirname, '../src/'), resolve(__dirname, '../public/')],
+
+    static: {
+      directory: resolve(__dirname, '../dist'),
+    },
+
+    devMiddleware: {
+      index: true,
+      publicPath: '/',
+
+      /** If you want to view files */
+      // writeToDisk: true,
+    },
+
+    client: {
+      progress: true,
+      logging: 'log',
+      overlay: true,
+    },
+
+    proxy: {
+      '/api': {
+        target: process.env.BACKEND_URL,
+        secure: false,
+        changeOrigin: true,
+        logLevel: 'debug',
+      },
+    },
+  },
 };
 
 module.exports = () => config;
