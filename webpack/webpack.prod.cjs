82d163a00a094e519f5a6d08ae3f471a3e0c11bf
EliotPu <78807845+EliotPu@users.noreply.github.com>
2023-07-12 10:52:52 +0300

    build(webpack-prod): add output

--- webpack/webpack.prod.cjs
+++ webpack/webpack.prod.cjs
@@ -3,6 +3,11 @@ console.log('webpack-prod-js');
 
 const config = {
   mode: 'production',
+
+  output: {
+    filename: '[name].[contenthash].bundle.mjs',
+    chunkFilename: '[name].[contenthash].mjs',
+  },
 };
 
 module.exports = () => config;
